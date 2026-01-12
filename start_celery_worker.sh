beca46447f1f7062a41f5f5b3977e3745e2026f2
mpn199/linnuocheng <linnuocheng@freeuuu.com>
2026-01-12 17:46:29 +0800

    修改test环境变量

--- start_celery_worker.sh
+++ start_celery_worker.sh
@@ -9,5 +9,4 @@ cd "$(dirname "$0")/src"
 
 # 启动 Celery worker
 
-conda run -n video_mix python -m celery -A celery_mq.celery_app worker --hostname=celery_local@%h --loglevel=INFO --queues=video_queue_dev --concurrency=4
-
+conda run -n test_gpu python -m celery -A celery_mq.celery_app worker --hostname=celery_test@%h --loglevel=INFO --queues=video_queue_test --concurrency=4
