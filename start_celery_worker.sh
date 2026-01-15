18c36a40ceda90f642e4ffb5c37fb700bfc02255
SpadeAE86 <3812678292@qq.com>
2026-01-15 17:45:18 +0800

    test

--- start_celery_worker.sh
+++ start_celery_worker.sh
@@ -6,7 +6,8 @@
 
 # 进入 src 目录
 cd "$(dirname "$0")/src"
-
+pkill -f "celery -A celery_mq.celery_app"
 # 启动 Celery worker
 
 /root/miniconda3/envs/test_gpu/bin/python -m celery -A celery_mq.celery_app worker --hostname=celery_test@%h --loglevel=INFO --queues=video_queue_test --concurrency=4
+
