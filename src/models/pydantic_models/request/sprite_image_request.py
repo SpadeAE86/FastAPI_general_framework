c07b99cfe290ff5d5e0985e588764b37ad45a272
SpadeAE86 <3812678292@qq.com>
2026-01-30 14:55:37 +0800

    merge

--- src/models/pydantic_models/request/sprite_image_request.py
+++ src/models/pydantic_models/request/sprite_image_request.py
@@ -1,8 +1,9 @@
 from typing import *
 
-from pydantic import Field
+from pydantic import Field, field_validator
 
 from models.pydantic_models.request.base_request import BaseRequest
+from utils.obs_utils import obs_key_exists
 
 
 # 定义雪碧图请求体
@@ -12,4 +13,9 @@ class SpriteImageRequest(BaseRequest):
         description="视频链接"
     )
 
-
+    @field_validator("obs_video_path")
+    @classmethod
+    def validate_obs_video_path(cls, v: str) -> str:
+        if not obs_key_exists(v):
+            raise ValueError("OBS 视频不存在或无访问权限")
+        return v
